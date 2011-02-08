2d4e9dbdef2eaeea74067169b815d2eab6fd23ef
hermawanadhis <hermawanadhis@gmail.com>
2011-02-08 16:23:24 +0700

    delete some pref

--- others/user.js
+++ others/user.js
@@ -1,55 +1,50 @@
-/* My daily firefox user.js
+/* My firefox user.js
  * 05022011
- * If you have another user.js conf feel free to email me (hermawanadhis@gmail.com)
+ * If you have another user.js conf let me know it (hermawanadhis@gmail.com)
  */
+// browser
 user_pref("browser.blink_allowed",false);
-user_pref("browser.cache.disk.capacity",0);
-user_pref("browser.cache.disk.enable",true);
-user_pref("browser.cache.disk.smart_size.enabled",false);
-user_pref("browser.cache.disk_cache_ssl",true);
-user_pref("browser.cache.memory.capacity",-1);
-user_pref("browser.cache.memory.enable", true);
-user_pref("browser.ctrlTab.previews",true);
 user_pref("browser.display.force_inline_alttext",true);
-user_pref("browser.display.show_image_placeholders",true);
+user_pref("browser.download.dir",E:\temp);
 user_pref("browser.download.manager.scanWhenDone",false);
 user_pref("browser.download.manager.showAlertOnComplete",false);
-user_pref("browser.enable_automatic_image_resizing",true);
-user_pref("browser.frames.enabled",true);
+user_pref("browser.download.manager.showWhenStarting",false);
+user_pref("browser.feeds.showFirstRunUI",false);
+user_pref("browser.formfill.enable",false);
 user_pref("browser.fullscreen.autohide",false);
-user_pref("browser.sessionhistory.max_total_viewers", -1);
-user_pref("browser.taskbar.previews.enable",true);
-user_pref("content.interrupt.parsing",true);
-user_pref("content.max.tokenizing.time",36000);
-user_pref("content.notify.backoffcount",-1);
-user_pref("content.notify.interval",12000);
-user_pref("content.notify.ontimer",true);
-user_pref("content.switch.threshold",75000);
-user_pref("extensions.alwaysUnpack",false);
+user_pref("browser.startup.page",0);
+user_pref("extensions.checkCompatibility",false);
+user_pref("general.autoScroll",false);
 user_pref("general.smoothScroll",true);
-user_pref("geo.enabled",true);
-user_pref("gfx.direct2d.force-enabled",true);
-user_pref("gfx.downloadable_fonts.enabled",true);
-user_pref("gfx.font_rendering.directwrite.enabled",true);
-user_pref("gfx.use_text_smoothing_setting",false);
-user_pref("html5.enable",true);
-user_pref("layers.acceleration.force-enabled",true);
 user_pref("layout.spellcheckDefault",0);
-user_pref("network.dns.disablePrefetch",false);
-user_pref("network.http.keep-alive",true);
+user_pref("privacy.donottrackheader.enabled",true);
+user_pref("security.dialog_enable_delay",0);
+user_pref("browser.tabs.animate",false);
+user_pref("browser.panorama.animate_zoom",false);
+user_pref("browser.preferences.animateFadeIn",false);
+// firefox 4
+user_pref("gfx.direct2d.force-enabled",true);
+user_pref("gfx.font_rendering.directwrite.enabled",true);
+user_pref("layers.acceleration.force-enabled",true);
+user_pref("webgl.force-enabled",true);
+// performance connection
 user_pref("network.http.max-connections",48);
 user_pref("network.http.max-connections-per-server",24);
 user_pref("network.http.max-persistent-connections-per-proxy",16);
 user_pref("network.http.max-persistent-connections-per-server",8);
 user_pref("network.http.pipelining",true);
 user_pref("network.http.pipelining.maxrequests",8);
-user_pref("network.http.proxy.keep-alive",true);
 user_pref("network.http.proxy.pipelining",true);
 user_pref("network.http.request.max-start-delay",5);
-user_pref("network.prefetch-next",true);
-user_pref("network.websocket.enabled",true);
-user_pref("nglayout.enable_drag_images",true);
+// performance browser
+user_pref("browser.cache.disk.capacity",0);
+user_pref("browser.cache.disk.enable",true);
+user_pref("browser.cache.disk.smart_size.enabled",false);
+user_pref("browser.cache.memory.enable", true);
+user_pref("content.interrupt.parsing",true);
+user_pref("content.max.tokenizing.time",36000);
+user_pref("content.notify.backoffcount",-1);
+user_pref("content.notify.interval",12000);
+user_pref("content.notify.ontimer",true);
+user_pref("content.switch.threshold",75000);
 user_pref("nglayout.initialpaint.delay",120);
-user_pref("security.dialog_enable_delay",0);
-user_pref("svg.smil.enabled",true);
-user_pref("webgl.force-enabled",true);
