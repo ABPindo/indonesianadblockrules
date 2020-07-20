f27da072ea894157bd10088f89862db5f63de01a
gmarcher <gm.kecil@gmail.com>
2020-07-20 11:18:04 +0700

    202007200416

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ python tools/FOP.py
 
 # render it and save it into
 flrender -i abpindo=. abpindo.template subscriptions/abpindo.txt
-flrender -i abpindo=. abpindo_host.template subscriptions/host.txt
+flrender -i abpindo=. abpindo_hosts.template subscriptions/hosts.txt
 flrender -i abpindo=. abpindo_noannoyance.template subscriptions/abpindo_noannoyance.txt
 flrender -i abpindo=. abpindo_noelemhide.template subscriptions/abpindo_noelemhide.txt
 
