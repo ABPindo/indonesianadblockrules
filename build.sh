88414308e357b2704fa3c9becbc8eb010c691d6b
gmarcher <gm.kecil@gmail.com>
2021-12-26 19:05:30 +0700

    https://github.com/ABPindo/indonesianadblockrules/issues/186

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ python tools/FOP.py
 
 # render it and save it into
 flrender -i abpindo=. abpindo.template subscriptions/abpindo.txt
-flrender -i abpindo=. abpindo_hosts.template subscriptions/hosts.txt
+flrender -i abpindo=. abpindo_hosts.template subscriptions/abpindo_hosts.txt
 flrender -i abpindo=. abpindo_noannoyance.template subscriptions/abpindo_noannoyance.txt
 flrender -i abpindo=. abpindo_noelemhide.template subscriptions/abpindo_noelemhide.txt
 
+adblock2hosts --ip 0.0.0.0 -o subscriptions/hosts.txt subscriptions/abpindo_hosts.txt
